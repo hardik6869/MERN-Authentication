5c34bd1f73e253a2b731ae2950943adbf6dd1ff8
hardik6869 <sorthiyahardik6869@gmail.com>
2024-03-19 13:21:41 +0530

    Feat: Add Login Route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -27,4 +27,25 @@ const signup = async (req, res, next) => {
   return res.status(200).json({ message: user });
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return new Error(error);
+  }
+
+  if (!existingUser) {
+    return res.status(400).json({ message: "User not found. Signup Please!" });
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Invalid Email / Password" });
+  }
+  return res.status(200).json({ message: "SuccessFully LoggedIn" });
+};
+
 exports.signup = signup;
+exports.login = login;
