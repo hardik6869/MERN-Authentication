b1844d7ebad6a9edcec76ad4a03e9c7f1053e8c6
hardik6869 <sorthiyahardik6869@gmail.com>
2024-03-19 11:10:56 +0530

    Connect with MongoDB Database & Create Folder Structure

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use("/", (req, res, next) => {
-  res.send("hello");
-});
-
-app.listen(5000, () => {
-  console.log("listening to localhost 5000");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database is Connected! Listening to localhost 5000.");
+  })
+  .catch((err) => console.log(err));
