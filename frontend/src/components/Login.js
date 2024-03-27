43c0bc3c2d5163159017afe649073fa84bd9759f
hardik6869 <sorthiyahardik6869@gmail.com>
2024-03-27 12:01:11 +0530

    Implement Login Page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, TextField, Button, Typography } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { setupLoginUser } from "../utils/apiPaths";
 
 function Login() {
-  return <div>Login Page</div>;
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const sendRequest = async () => {
+    console.log(inputs);
+    const res = await axios
+      .post(setupLoginUser, inputs)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    console.log(data);
+    return data;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // send http request
+    sendRequest().then(() => navigate("/user"));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box
+        marginLeft="auto"
+        marginRight="auto"
+        width={300}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h2" color="initial">
+          Login
+        </Typography>
+
+        <TextField
+          name="email"
+          type="email"
+          value={inputs.email}
+          variant="outlined"
+          placeholder="Email"
+          margin="normal"
+          onChange={handleChange}
+        />
+        <TextField
+          name="password"
+          type="password"
+          value={inputs.password}
+          variant="outlined"
+          placeholder="Password"
+          margin="normal"
+          onChange={handleChange}
+        />
+        <Button variant="contained" type="submit">
+          Signup
+        </Button>
+      </Box>
+    </form>
+  );
 }
 
 export default Login;
--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,1 +1,2 @@
 export const setupSinupUser = `${process.env.REACT_APP_BASE_URL}/api/signup`;
+export const setupLoginUser = `${process.env.REACT_APP_BASE_URL}/api/login`;
