29895f36ae67b5ab125bf48652fd92ad400c3102
hardik6869 <sorthiyahardik6869@gmail.com>
2024-03-29 16:39:41 +0530

    backend: Implement Logout Functionaity

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -129,8 +129,27 @@ const refreshToken = async (req, res, next) => {
   });
 };
 
+const logout = (req, res, next) => {
+  const cookies = req.headers.cookie;
+  const prevToken = cookies?.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(String(prevToken), process.env.JWT_SECRET_KEY, (error, user) => {
+    if (error) {
+      console.log(error);
+      return res.status(403).json({ message: "Authentication failed" });
+    }
+    res.clearCookie(`${user.id}`);
+    req.cookies[`${user.id}`] = "";
+
+    return res.status(200).json({ message: "Successfully Logged out" });
+  });
+};
+
 exports.signup = signup;
 exports.login = login;
 exports.verifyToken = verifyToken;
 exports.getUser = getUser;
 exports.refreshToken = refreshToken;
+exports.logout = logout;
